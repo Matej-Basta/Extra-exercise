9a2552f171a880a18c996643f4401d76e34602ee
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-31 12:46:45 +0100

    function that returns the last element of an array without using length method

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 //function that returns the length of an array without using length method
 const len = (arr) => {
   let count = 0;
-  for (let i = 0; i < arr.length; i++) {
+
+  arr.forEach((element) => {
     count++;
-  }
+  });
   console.log(count);
 };
 
 len([0]);
 len([5, 4, 3, 6, 7]);
+len([0, 0, 0]);
+
+//function that returns the last element of an array without using length method
+const last = (arr) => {
+  let number = null;
+  arr.forEach((element) => {
+    number = element;
+  });
+  console.log(number);
+};
+
+last([0]);
+last([5, 4, 3, 6, 7]);
+last([]);
